5084f0709912410006ff58668b92dd35c01ea84a
helloyou2012 <helloyou2012@yeah.net>
2021-04-12 15:24:37 +0800

    bug fixed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const defaultOptions = {
 
 module.exports = (api, options) => {
   options = options || {};
-  const plugins = [require('babel-plugin-syntax-top-level-await')];
+  const plugins = [require('@babel/plugin-syntax-top-level-await')];
   proposals.forEach((key) => {
     const name = `@babel/plugin-proposal-${key}`;
     const value = options[key] || options[name];
